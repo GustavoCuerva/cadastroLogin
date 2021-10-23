c82d76d441623fdb519d6082356be2dc2b93e519
Gustavo Cuerva <cuervagustavo@gmail.com>
2021-10-23 14:22:22 -0300

    Adicionando banco de dados

---
+++ db/dados.sql
@@ -0,0 +1,9 @@
+create database cadastro;
+use cadastro;
+
+CREATE TABLE `cadastro`.`dadosUsuario` (
+  `id` INT NOT NULL,
+  `nome` VARCHAR(90) NOT NULL,
+  `email` VARCHAR(50) NOT NULL,
+  `senha` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`));
