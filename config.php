2c9aa2a1059b041741474c4c76c8bdf8a2e290e2
Gustavo Cuerva <cuervagustavo@gmail.com>
2021-10-29 18:44:18 -0300

    Conexao com banco de dados

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+    $dbHost = 'Localhost';
+    $dbUsername = 'root';
+    $dbPassowrd = '1234';
+    $dbName = 'cadastro';
+
+    $conexao = new mysqli($dbHost,$dbUsername,$dbPassowrd,$dbName);
+?>
--- db/dados.sql
+++ db/dados.sql
@@ -2,7 +2,7 @@ create database cadastro;
 use cadastro;
 
 CREATE TABLE `cadastro`.`dadosUsuario` (
-  `id` INT NOT NULL,
+  `id` INT NOT NULL AUTO_INCREMENT,
   `nome` VARCHAR(90) NOT NULL,
   `email` VARCHAR(50) NOT NULL,
   `senha` VARCHAR(45) NOT NULL,
